fc0e393126a8343c7ba95e603fa428e1202fbacf
Matthew Lynden <mjlynden@gmail.com>
2019-05-24 12:29:18 +1000

    Updated provider configuration with credential variables

--- provider.tf
+++ provider.tf
@@ -1,5 +1,7 @@
 
 # AWS provider configuration
 provider "aws" {
+  access_key = "${var.AWS_ACCESS_KEY}" # See terraform.tfvars
+  secret_key = "${var.AWS_SECRET_KEY}" # See terraform.tfvars
   region     = "${var.AWS_REGION}" # See vars.tf
 }
