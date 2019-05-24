755f4f581f429dc6a6adb941057042256a0c7df6
Matthew Lynden <mjlynden@gmail.com>
2019-05-24 12:57:09 +1000

    Updates to vars.tf for credentials

--- vars.tf
+++ vars.tf
@@ -1,5 +1,7 @@
 
-# Variable definition configuration
+# AWS provider variable definition
+variable "AWS_ACCESS_KEY" {} # See terraform.tfvars
+variable "AWS_SECRET_KEY" {} # See terraform.tfvars
 variable "AWS_REGION" {
   description = "The default AWS region"
   default     = "ap-southeast-2" # Asia Pacific (Sydney)
