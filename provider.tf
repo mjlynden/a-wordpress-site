c9c9a4b4d20799e3a9babb30604e61c665d0e8ca
Matthew Lynden <mjlynden@gmail.com>
2019-05-24 11:57:28 +1000

    Configure aws provider

---
+++ provider.tf
@@ -0,0 +1,5 @@
+
+# AWS provider configuration
+provider "aws" {
+  region     = "${var.AWS_REGION}" # See vars.tf
+}
---
+++ vars.tf
@@ -0,0 +1,6 @@
+
+# Variable definition configuration
+variable "AWS_REGION" {
+  description = "The default AWS region"
+  default     = "ap-southeast-2" # Asia Pacific (Sydney)
+}
